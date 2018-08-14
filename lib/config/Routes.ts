eea75d9320c4fd02874a848a7980b57b31b9cc13
Jordy Baylac <jbaylac@farelogix.com>
2018-08-14 16:27:04 -0400

    refactor router

--- lib/config/Routes.ts
+++ lib/config/Routes.ts
@@ -21,18 +21,7 @@ export class Routes {
         // Tournament
         app
             .route('/tournament')
-            .get((req: Request, res: Response, next: NextFunction) => {
-                // middleware
-                console.log(`Request from: ${req.originalUrl}`);
-                console.log(`Request type: ${req.method}`);
-                if (req.query.key !== '78942ef2c1c98bf10fca09c808d718fa3734703e') {
-                    res
-                        .status(HttpStatus.UNAUTHORIZED)
-                        .send('You shall not pass!');
-                } else {
-                    next();
-                }
-            }, this.tournamentController.getTournaments.bind(this.tournamentController))
+            .get(this.newMethod(), this.tournamentController.getTournaments.bind(this.tournamentController))
 
             // POST endpoint
             .post(this.tournamentController.addNewTournament.bind(this.tournamentController));
@@ -44,5 +33,20 @@ export class Routes {
             .put(this.tournamentController.updateTournament.bind(this.tournamentController))
             .delete(this.tournamentController.deleteTournament.bind(this.tournamentController));
 
+    }
+
+    private checkSecurity(req: Request, res: Response, next: NextFunction): void  {
+        
+            // middleware
+            console.log(`Request from: ${req.originalUrl}`);
+            console.log(`Request type: ${req.method}`);
+            if (req.query.key !== '78942ef2c1c98bf10fca09c808d718fa3734703e') {
+                res
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .send('You shall not pass!');
+            }
+            else {
+                next();
+            }
     }
 }
